9b3cd7bbeb98aae4f9bf7702926a2d1429201ae3
Kunal Telangi <74617472+kunaltelangi@users.noreply.github.com>
2025-08-16 22:47:45 +0530

    Update main.py

--- prodat/cli/main.py
+++ prodat/cli/main.py
@@ -10,92 +10,72 @@ from prodat.core.util.i18n import get as __
 from prodat.core.util.logger import prodatLogger
 from prodat.config import Config
 
+
 def main():
     cli_helper = Helper()
-    # Config is required to run first so it can
-    # initialize/find prodat home directory (.prodat)
-    # This is required for logging to place the logs in a
-    # place for the user.
+
     config = Config()
     config.set_home(os.getcwd())
 
     log = prodatLogger.get_logger(__name__)
     log.info("handling command %s", config.home)
 
-    # parse_args defaults to [1:] for args, but you need to
-    # exclude the rest of the args too, or validation will fail
-    # args = parser.parse_args(sys.argv[1:2])
-    if len(sys.argv) > 1 and \
-        sys.argv[1] in cli_helper.get_command_choices():
-        command_name = sys.argv[1]
-        # commands in project.py
-        if command_name == "init":
-            command_name = "project"
-        elif command_name == "version" or \
-            command_name == "--version" or \
-            command_name == "-v":
-            command_name = "project"
-            sys.argv[1] = "version"
-        elif command_name == "status":
-            command_name = "project"
-            sys.argv[1] = "status"
-        elif command_name == "cleanup":
-            command_name = "project"
-            sys.argv[1] = "cleanup"
-        elif command_name == "dashboard":
-            command_name = "project"
-            sys.argv[1] = "dashboard"
-        elif command_name == "configure":
-            command_name = "project"
-            sys.argv[1] = "configure"
-        # commands in workspace.py
-        elif command_name in ["notebook", "jupyterlab", "terminal", "rstudio"]:
-            sys.argv[1] = command_name
+    command_name = None
+
+    if len(sys.argv) > 1 and sys.argv[1] in cli_helper.get_command_choices():
+        cmd = sys.argv[1]
+
+        project_cmds = {
+            "init": "project",
+            "version": "project",
+            "--version": "project",
+            "-v": "project",
+            "status": "project",
+            "cleanup": "project",
+            "dashboard": "project",
+            "configure": "project",
+        }
+
+        workspace_cmds = {"notebook", "jupyterlab", "terminal", "rstudio"}
+        run_cmds = {"rerun", "run", "stop", "ls", "delete"}
+
+        if cmd in project_cmds:
+            command_name = project_cmds[cmd]
+            if cmd in {"version", "--version", "-v", "status", "cleanup", "dashboard", "configure"}:
+                sys.argv[1] = cmd
+        elif cmd in workspace_cmds:
             command_name = "workspace"
-        # commands in run.py
-        elif command_name == "rerun":
+            sys.argv[1] = cmd
+        elif cmd in run_cmds:
             command_name = "run"
-            sys.argv[1] = "rerun"
-        elif command_name == "run":
-            if len(sys.argv) == 2:
-                command_name = "run"
+            if cmd in {"run", "stop"} and len(sys.argv) == 2:
                 sys.argv.append("--help")
-            else:
-                command_name = "run"
-        elif command_name == "stop":  # stop command in run.py
-            if len(sys.argv) == 2:
-                command_name = "run"
-                sys.argv.append("--help")
-            else:
-                command_name = "run"
-        elif command_name == "ls":  # ls command in run.py
-            command_name = "run"
-        elif command_name == "delete":  # delete command in run.py
-            command_name = "run"
+            sys.argv[1] = cmd
+        else:
+            command_name = cmd
+
         command_class = cli_helper.get_command_class(command_name)
+
     elif len(sys.argv) == 1:
-        command_name = "prodat_command"
-        command_class = cli_helper.get_command_class(command_name)
+        command_class = cli_helper.get_command_class("prodat_command")
     else:
         command_class = BaseCommand
 
-    # instantiate the command class
     try:
         command_instance = command_class(cli_helper)
     except TypeError as ex:
-        cli_helper.echo(__("error", "cli.general", "%s %s" % (type(ex), ex)))
+        cli_helper.echo(__("error", "cli.general", f"{type(ex)} {ex}"))
         return 1
 
-    # parse the command line arguments
     try:
         command_instance.parse(sys.argv[1:])
     except CLIArgumentError as ex:
-        cli_helper.echo(__("error", "cli.general", "%s %s" % (type(ex), ex)))
+        cli_helper.echo(__("error", "cli.general", f"{type(ex)} {ex}"))
         return 1
 
     try:
         command_instance.execute()
         return 0
     except Exception as ex:
-        cli_helper.echo(__("error", "cli.general", "%s %s" % (type(ex), ex)))
+        cli_helper.echo(__("error", "cli.general", f"{type(ex)} {ex}"))
         return 1
